7cfc2d16ac9a9970bcd108197fc219f7e18fd430
JBBSoftech <jbbsoftech@gmail.com>
2025-10-28 21:04:58 +0530

    Update Appifyours/frontend/lib/main.dart

--- Appifyours/frontend/lib/main.dart
+++ Appifyours/frontend/lib/main.dart
@@ -340,7 +340,7 @@ class _HomePageState extends State<HomePage> {
                         
                         const SizedBox(width: 8),
                         Text(
-                          'mohan',
+                          'mohan raj',
                           style: const TextStyle(
                             color: Colors.white,
                             fontWeight: FontWeight.bold,
